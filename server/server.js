af65728e8a863c7663504935a4f411ec8e905c3e
Ismael De los Santos <ismaeldso125@gmail.com>
2024-09-28 12:54:24 -0400

    Final Commit

---
+++ server/server.js
@@ -0,0 +1,49 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import OpenAI from "openai"
+
+dotenv.config({path: '../config.env'})
+
+const openai = new OpenAI({
+    apiKey: "sk-mpQktAIUC8J-7w04vytghaAHS5suyBQ0g_DxTJ-sk_T3BlbkFJzf7MrIg_7UN_HhS1jJ34EXsbw2vdyBs_CwPyRTP4IA"
+})
+
+const app = express()
+app.use(cors({origin:"*"}))
+app.use(express.json());
+
+app.get("/", async (req,res) =>{
+    res.status(200).send({
+        message:"From the Endpoint"
+    })
+})
+
+app.post("/", async (req,res) =>{
+    try {
+        const prompt = req.body.prompt;
+        const response = await openai.completions.create({
+            model: "gpt-3.5-turbo-0125",
+            prompt: prompt,
+            temperature: 1,
+            max_tokens: 2048,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            response_format: {
+                "type": "text"
+            },
+          });
+          console.log(response.data)
+          res.status(200).send({
+            bot: response.data.choices[0].text
+          })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            message: error
+        })
+    }
+})
+
+app.listen(3000, () => console.log("Server is running on port http://localhost:3000"))
